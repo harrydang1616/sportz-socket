609972acee7cbc3b23f6bac9c4dc17b52d0bdad0
Harry Dang <harrydang.evolux@gmail.com>
2026-02-27 14:54:19 +0700

    first config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
-const port = 8000;
+const PORT = 8000;
 
+// Use JSON middleware
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express Server");
+// Root GET route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the Sportz Express Server!' });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
