afe147aa1ea226feef5cce912c4833d81f14a7ed
Harry Dang <harrydang.evolux@gmail.com>
2026-03-02 13:47:33 +0700

    implement matches POST and GET routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {matchRouter} from "./routes/matches.js";
 
 const app = express();
 const PORT = 8000;
@@ -8,10 +9,12 @@ app.use(express.json());
 
 // Root GET route
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the Sportz Express Server!' });
+    res.send({message: 'Welcome to the Sportz Express Server!'});
 });
+
+app.use('/matches', matchRouter)
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
